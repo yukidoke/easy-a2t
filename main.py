90619a97c7a689372f747ee7c407c62c286b5858
yukidoke <toyo.kira.0115@gmail.com>
2026-04-17 00:26:10 +0900

    文字起こしの動作を確認

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def get_transcribed_text(file: UploadFile):
         language="ja",
         vad_filter=True,
         vad_parameters=dict(
-            min_silence_duratiton_ms=500,
+            min_silence_duration_ms=500,
         ),
         beam_size=5,
         without_timestamps=False
@@ -24,10 +24,11 @@ def get_transcribed_text(file: UploadFile):
         for segment in segments:
             print(f"[{segment.start:.2f} -> {segment.end:.2f}] {segment.text}", file=f)
 
-@app.post("/uploadvoices/")
+@app.post("/uploadvoices")
 async def create_upload_files(files: list[UploadFile], background_tasks: BackgroundTasks):
     for file in files:
         background_tasks.add_task(get_transcribed_text, file=file)
+        print("Task event")
     return {"filenames": [file.filename for file in files]}
 
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
