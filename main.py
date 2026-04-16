73d65334138b304f356173075f6b860b26e2a6cf
yukidoke <toyo.kira.0115@gmail.com>
2026-04-17 01:11:14 +0900

    QRコードの生成に失敗

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
+import io
 import psutil
+import qrcode
+import socket
 from fastapi import BackgroundTasks, FastAPI, UploadFile
 from fastapi.staticfiles import StaticFiles
 from faster_whisper import WhisperModel
+from starlette.responses import StreamingResponse
 
 model_size = "small"
+
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+    return ip
+
+ip_address = get_local_ip()
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.ERROR_CORRECT_L,
+    box_size=10,
+    border=4
+)
+qr.add_data("http://" + ip_address + ":8000")
+qr.make(fit=True)
+img = qr.make_image(fill="black", back_color="white")
 
 model = WhisperModel(model_size, device="cpu", compute_type="int8", cpu_threads=psutil.cpu_count(logical=False))
 
@@ -30,6 +56,12 @@ async def create_upload_files(files: list[UploadFile], background_tasks: Backgro
         background_tasks.add_task(get_transcribed_text, file=file)
         print("Task event")
     return {"filenames": [file.filename for file in files]}
+
+@app.get("/qr")
+def image():
+    img_bytes = io.BytesIO()
+    img.get_image().save(img_bytes, format="PNG")
+    return StreamingResponse(img_bytes.getvalue(), media_type="image/png")
 
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
